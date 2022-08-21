42cdab4216ddddf3bdbfc59ce7b376e04982e0d5
VIP BOY <103081103+itsvipboy1@users.noreply.github.com>
2022-08-21 12:44:17 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -106,7 +106,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
     draw.text(
         (190, 670),
-        f"Powered By: 𝄟🐰❤️⃝🇻​​​​​🇮​​​​​≛⃝🇵​​​​​💞🐰𝄟 (@THE_VIP_BOY)",
+        f"Powered By: ꧁ VIP BOY ꧂ (@THE_VIP_BOY)",
         (255, 255, 255),
         font=font,
     )
